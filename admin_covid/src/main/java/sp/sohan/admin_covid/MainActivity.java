8c7d343f74e3d924c09b1caf0944491f3d8866b5
Sohan Patil <131578900+Sohan-04-P@users.noreply.github.com>
2023-12-27 18:54:50 +0530

    Add files via upload

---
+++ admin_covid/src/main/java/sp/sohan/admin_covid/MainActivity.java
@@ -0,0 +1,16 @@
+package sp.sohan.admin_covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ay.ayush.admin_covid.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
